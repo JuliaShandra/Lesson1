1da40cf202650e633c9b01f17dd1d0337aadf0ba
JuliaShandra <63914084+JuliaShandra@users.noreply.github.com>
2020-08-20 01:22:31 +0300

    creat html and js

---
+++ script.js
@@ -0,0 +1,50 @@
+r = prompt ('Какой радиус цилиндра?', '');
+h = prompt ('Какая высота цилиндра?', '');
+s = (r * r) * Math.PI;
+v = s * h;
+
+alert(`Обьем цилиндра с площадью основы:${s}, радиусом:${r} и высотой:${h} равен:${v} `);
+
+alert(`V =${v}`);
+
+
+
+// console.log( );
+// document.write();
+
+// x = 24; // Number
+// x = '444dfbvhjdfbv'; // String
+// x = parseInt(x);
+// x = +x;
+
+// y = 20;
+
+// console.log(x);
+
+// y = '2.346';
+// y = parseFloat(y);
+// console.log(y);
+
+// y = 10;
+
+// z = x - y;
+
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+// console.log(x + y);
+
+// console.log(y);
+
+// console.log(z);
+// console.log(x,y,z);
+
+// document.write('<p>'+x+'</p>');
+// document.write('<div>'+y+'</div>');
+// document.write(z);
+
+
+
+
